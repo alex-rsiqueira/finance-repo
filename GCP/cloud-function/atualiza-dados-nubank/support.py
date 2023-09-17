dbfdd3b403f431667f4ef413fe514e25aa65d93c
Aleesiqueira <alexsander_siqueira@outlook.com>
2023-09-17 01:42:35 +0100

    Retrieve certificate from Cloud Storage

--- GCP/cloud-function/atualiza-dados-nubank/support.py
+++ GCP/cloud-function/atualiza-dados-nubank/support.py
@@ -12,24 +12,12 @@ def read_secret(secret_name):
 
     return secret_value
 
-def download_blob(bucket_name, source_blob_name, destination_file_name):
+def download_blob(bucket_name, source_blob_name):
     """Downloads a blob from the bucket."""
-    # The ID of your GCS bucket
-    bucket_name = "your-bucket-name"
-
-    # The ID of your GCS object
-    # source_blob_name = "storage-object-name"
-
-    # The path to which the file should be downloaded
-    # destination_file_name = "local/path/to/file"
 
     storage_client = storage.Client()
 
     bucket = storage_client.bucket(bucket_name)
 
-    # Construct a client side representation of a blob.
-    # Note `Bucket.blob` differs from `Bucket.get_blob` as it doesn't retrieve
-    # any content from Google Cloud Storage. As we don't need additional data,
-    # using `Bucket.blob` is preferred here.
     blob = bucket.blob(source_blob_name)
-    blob.download_to_filename(destination_file_name)
+    blob.download_to_filename(source_blob_name)
