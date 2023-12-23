5e3c75ce421a2bbd8ddf767cb0209618adeddf69
Aleesiqueira <alexsander_siqueira@outlook.com>
2023-12-23 11:41:33 -0600

    Add prints to debug Nordigen ingestion

--- GCP/cloud-function/atualiza-dados-nordigen/connection.py
+++ GCP/cloud-function/atualiza-dados-nordigen/connection.py
@@ -39,6 +39,7 @@ def read_secret(secret_name):
 def get_nordigen_client(secret_id,secret_key):
 
     # initialize Nordigen client and pass SECRET_ID and SECRET_KEY
+    print('Initialize Nordigen client')
     client = NordigenClient(
         secret_id=secret_id,
         secret_key=secret_key
@@ -47,21 +48,19 @@ def get_nordigen_client(secret_id,secret_key):
     # Create new access and refresh token
     # Parameters can be loaded from .env or passed as a string
     # Note: access_token is automatically injected to other requests after you successfully obtain it
+    print('Generate token')
     token_data = client.generate_token()
-
-    # Use existing token
-    #client.token = "YOUR_TOKEN"
 
     # Exchange refresh token for new access token
     new_token = client.exchange_token(token_data["refresh"])
 
     # Get existing requisitions
+    print('Check for existing requisitions')
     requisitions = client.requisition.get_requisitions()
 
     if not any([x['status'] == 'LN' for x in requisitions['results']]): # Check if any existing requisition has the status LN - Linked
 
-        # Get all institution by providing country code in ISO 3166 format
-        #institutions = client.institution.get_institutions("PT")
+        print('Open new requisition')
 
         # Get institution id by bank name and country
         institution_id = client.institution.get_institution_id_by_name(
@@ -70,6 +69,7 @@ def get_nordigen_client(secret_id,secret_key):
         )
 
         # Initialize bank session
+        print('Initialize bank session')
         init = client.initialize_session(
             # institution id
             institution_id=institution_id,
@@ -79,18 +79,16 @@ def get_nordigen_client(secret_id,secret_key):
             reference_id=str(uuid4())
         )
 
-        # Get requisition_id and link to initiate authorization process with a bank
-        # link = init.link # bank authorization link
-        # requisition_id = init.requisition_id
-
         # Get account id after you have completed authorization with a bank
         # requisition_id can be gathered from initialize_session response
+        print('Get new requisition ID')
         accounts = client.requisition.get_requisition_by_id(
             requisition_id=init.requisition_id
         )
 
     else:
 
+        print('Get active requisition ID')
         # Get account list from last authenticated requisition
         accounts = client.requisition.get_requisition_by_id(
             requisition_id = [x['id'] for x in requisitions['results'] if x['status'] == 'LN'][0]
