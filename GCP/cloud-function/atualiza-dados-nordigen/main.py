1ff444f7605f9d487c65eff1c28d09f4f2396395
Aleesiqueira <alexsander_siqueira@outlook.com>
2023-12-23 03:53:21 -0600

    Add prints to debug Nordigen ingestion

--- GCP/cloud-function/atualiza-dados-nordigen/main.py
+++ GCP/cloud-function/atualiza-dados-nordigen/main.py
@@ -5,19 +5,28 @@ TABLES_ID = ['tb_nordigen_transactions', 'tb_nordigen_meta', 'tb_nordigen_balanc
 
 def main(context):
     
-    nordigen_accounts = connection.get_nordigen_accounts()
+    try:
+        print('Get Nordigen connection')
+        nordigen_accounts = connection.get_nordigen_accounts()
 
-    for row in nordigen_accounts:
-        user_id = row['id']
-        secret_id = row['secret_ID']
-        secret_key = connection.read_secret('Nordigen_' + secret_id)
-        client, accounts = connection.get_nordigen_client(secret_id,secret_key)
+        print('Loop through accounts')
+        for row in nordigen_accounts:
+            user_id = row['id']
+            secret_id = row['secret_ID']
 
-        for i in range(0,len(accounts["accounts"])-1):
-            account_id = accounts["accounts"][i]
-            account = client.account_api(id=account_id)
+            print('Read Nordigen secret')
+            secret_key = connection.read_secret('Nordigen_' + secret_id)
+            
+            print('Get Nordigen client')
+            client, accounts = connection.get_nordigen_client(secret_id,secret_key)
 
-            for table_id in TABLES_ID:
-                print('<<>> Iniciando:', table_id, '<<>>')
-                support.get_data(user_id, account, table_id)
-                print('<<>>',  table_id, 'Finalizada <<>>')
+            for i in range(0,len(accounts["accounts"])-1):
+                account_id = accounts["accounts"][i]
+                account = client.account_api(id=account_id)
+
+                for table_id in TABLES_ID:
+                    print('<<>> Iniciando:', table_id, '<<>>')
+                    support.get_data(user_id, account, table_id)
+                    print('<<>>',  table_id, 'Finalizada <<>>')
+    except Exception as e:
+        print(f'Error : {e}')
